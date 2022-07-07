37859796294a2cb8beaab0a5a0976223c27ded4b
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-07-07 11:27:04 +0200

    Fix i

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Create global variables
+//* Create global variables
 const startBtn = document.getElementById("start-button");
 const startSection = document.getElementById("start-section");
 var quizTimer;
@@ -10,7 +10,7 @@ var isTrue;
 var isFalse;
 */
 
-//! Create array of questions, choices, and correct answers
+//* Create array of questions, choices, and correct answers //! not index number
 var questions = 
 [
     {
@@ -22,7 +22,7 @@ var questions =
         "dir", 
         "pwd"        
         ],
-    answer: 2
+    answer: 3
     },
     {
     question: "To fetch changes from a Github repo to merge with your local directory, use...",
@@ -33,7 +33,7 @@ var questions =
         "git push", 
         "git add", 
         ],
-    answer: 1
+    answer: 2
     },
     {
     question: "Which is invalid CSS?",
@@ -44,7 +44,7 @@ var questions =
         "&lt;div class= \"definition\"&gt;&lt;/div&gt;", 
         "&lt;div class= \"definition high\"&gt;&lt;/div&gt;", 
         ],
-    answer: 1
+    answer: 2
     },
     {
     question: "There are THREE ways to insert a style sheet: external CSS, internal CSS, and inline CSS.",
@@ -53,7 +53,7 @@ var questions =
         "True",
         "False",
         ],
-    answer: 0
+    answer: 1
     },
     {
     question: "Using document.querySelector( \"demo \").innerHTML =  \"Hello, JavaScript! \" will find an element with id=demo and change its content to say Hello, JavaScript!",
@@ -62,7 +62,7 @@ var questions =
         "True", 
         "False",
         ],
-    answer: 1
+    answer: 2
     },
     {
     question: "Select the correct Bootstrap class to set your element's background color to white.",
@@ -73,40 +73,11 @@ var questions =
         ".color-white",
         ".bg-primary",
         ],
-    answer: 1
+    answer: 2
     }
 ];
 
-//! On button click, start the quiz
-startBtn.onclick = function() {
-    runQuiz();
-}
-
-//! Start a timer and run a loop with the quiz questions
-function runQuiz() {    
-    console.log("The button is clicked!");
-    document.getElementById("start-section").classList.add("hide");
-    // Create container for question
-    createQASpace();
-    askQuestion(0);
-    var quizTimer = 60;
-    showTimer();
-    // ! Show timer countdown in seconds
-    var quizTimerEnd = 0;
-    console.log("The timer is now running!");
-    var countDown = setInterval(function() {
-        quizTimer--
-        if (quizTimer === 0) {
-            console.log("Time's up!");
-            clearInterval(countDown);
-            endQuiz();
-        } else {
-            document.getElementById("time-remaining").innerHTML = "<h2>Seconds remaining: " + quizTimer + "</h2>"
-        }
-    }, 1000);
-}
-
-//! Create the display area for questions and answers
+//* Create the display area for questions and answers
 function createQASpace() {
     document.getElementById("qa-section").classList.remove("hide")
     var qaDiv = document.getElementById("qa-section");
@@ -121,15 +92,7 @@ function createQASpace() {
     return
 }
 
-//! Create the display area for the right/wrong result statement
-function createRightWrongSpace() {
-    var isRightWrongDiv = document.createElement("div")
-    isRightWrongDiv.setAttribute("id", "right-wrong")
-    isRightWrongDiv.classList.add("right-wrong")
-    document.getElementById("qa-section").appendChild(isRightWrongDiv);
-}
-
-//! Create the display area for the timer
+//* Create the display area for the timer
 function showTimer() {
     var remainingTime = document.createElement("div")
     remainingTime.setAttribute("id", "time-remaining")
@@ -137,7 +100,53 @@ function showTimer() {
     document.getElementById("qa-section").appendChild(remainingTime);
 }
 
-// ! Draw questions and answers from the array
+//* On button click, start the quiz
+startBtn.onclick = function() {
+    runQuiz();
+}
+
+//! Start a timer and run a loop with the quiz questions
+function runQuiz() {    
+    console.log("The button is clicked!");
+    // Remove start section and replace with section for question
+    document.getElementById("start-section").classList.add("hide");
+    createQASpace();
+
+    // Ask the question
+    askQuestion(0); //! I think this is a problem because it is just the first question
+
+    // Start the timer
+    runTimer();
+}
+
+//* Start the timer at 60 seconds and show remaining time on screen
+function runTimer() {
+    var quizTimer = 60;
+    showTimer();
+    var quizTimerEnd = 0;
+    console.log("The timer is now running!");
+    var countDown = setInterval(function() {
+        quizTimer--
+        if (quizTimer === 0) {
+            console.log("Time's up!");
+            clearInterval(countDown);
+            endQuiz();
+        } else {
+            document.getElementById("time-remaining").innerHTML = "<h2>Seconds remaining: " + quizTimer + "</h2>"
+        }
+    }, 1000);
+}
+
+
+//! Create the display area for the right/wrong result statement <<---- haven't yet tested this
+function createRightWrongSpace() {
+    var isRightWrongDiv = document.createElement("div")
+    isRightWrongDiv.setAttribute("id", "right-wrong")
+    isRightWrongDiv.classList.add("right-wrong")
+    document.getElementById("qa-section").appendChild(isRightWrongDiv);
+}
+
+// ! Draw questions and answers from the array, display answers as buttons with corresponding id numbers
 function askQuestion() {
     // Ask question from array
             var question = questions[i].question;
@@ -150,18 +159,40 @@ function askQuestion() {
                 var choice = questions[i].choices[j];
                 li.innerHTML
                 //create buttons, make ids equal to index+1, and create onclick
-                += (`<li id="${j + 1}" onclick="recordID(this.id);"><button class="btn btn-primary btn-lg btn-grid">` 
+                += (`<li id="${j + 1}" onclick="recordID(this.id);"><button class="btn btn-primary btn-lg btn-grid">` //! note formatting
                 + choice 
                 + `</button></li>`);
-                console.log(j)
-                console.log(choice)
-        } 
-
-     // Increment i and proceed to next question
-        return i++
+        }
 }
 
-// ! End the quiz and show the results
+// ! On click, record the id of the selected choice and check against answer
+function recordID(id) {
+    var answerID = id;
+    console.log(answerID);
+    isCorrect()
+    return answerID;
+
+        function isCorrect() {
+            // the purpose of the function is to evaluate whether the input is the same as the answer 
+            console.log(answerID, "inside isCorrect function");
+            var userScore = 0;
+                if ( answerID == questions[i].answer ) {
+                console.log("Hurrah! Right choice!")
+                userScore =+ 10
+                console.log(userScore)
+                }
+                else {
+                console.log("Bummer! Better luck with the next one!")
+                userScore =- 10
+                console.log(userScore)
+                }
+                return userScore
+        }
+}
+
+//! Create function to tally score
+
+//* End the quiz, create a space for and show results
 
 function endQuiz() {
     document.getElementById("qa-section").classList.add("hide")
@@ -169,22 +200,4 @@ function endQuiz() {
     document.getElementById("times-up").innerHTML = "<h2>Time's up!</h2><br><p>Let's review your results.</p>"
         return console.log("This game is over!");
     //showScore and registerUser
-}
-
-// ! On click, record the id of the selected choice
-function recordID(id) {
-    var answerID = id;
-    return answerID;
-}
-
-
-// ! Hold these functions for later development
-function isCorrect() {
-    console.log("Correct answer!")
-    userScore += 10
-    return userScore
-}
-
-function isNotCorrect() {
-    console.log("Wrong answer! Move on!")
 }
